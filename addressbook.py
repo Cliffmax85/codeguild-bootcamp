e9f82f89c622e26cd246eca36d78e353e3381982
Clifford Maxson <cliffmax85@gmail.com>
2015-01-13 10:18:57 -0800

    Updated Version

--- addressbook.py
+++ addressbook.py
@@ -1,63 +1,86 @@
+import json 
+		
+file_name = "./contact_info.json" 		
+		
+		
+def load_file(book):
+	 
+	f = open(file_name, 'r')
+	#try:	
+	book.update(json.load(f))	
+	#except ValueError:
+		#print "Invalid Data"
+		#return
+	f.close()		
+		
+		
+		
+def delete_contact(book):
 
-		
-		
-		
-		
-		
-		
-address_book = []		
-		
-def delete_contact():
-	print address_book
-	
-	#delete contains name to delete
-	delete = raw_input("Enter name to be deleted: ")
-	#find entry name in list address_book
-	#that may succeed or fail. If don't find name print error
-	for delete in address_book:
-		if delete == 'quit':
-			
+	delete = raw_input("Enter name to be deleted")
+	try:
+		del book[delete] 
+	except KeyError:
+		print "No name in system"
+		return 
 		
 	
 	
 	
 	
-def add_to_list():
 	
-	contact = raw_input("Enter name first a comma and then your number ")
-	parts = {'contact_info' : contact}
-	parts = contact.split(',')
-	print parts
-	return parts 
+def add_to_list(book):
+	
+	contact = raw_input("Type out Name and phone number seperated by a comma: ")
+	contact_split = contact.split(",")
+	if len(contact_split) != 2:
+		print "Entered bad info"
+		return
+	book[contact_split[0]] = contact_split[1]
+	
+	
+def save_list(book):
+
+	f = open(file_name, 'w')
+	json.dump(book, f)
+	f.close()
+	
 
 
 def main():
+
+	address_book = {}
 	
 	while True:
 	
-		choice = raw_input("Enter address book command. Add, list, delete or quit:")
+		choice = raw_input("Enter address book command. Add, list, del, load and save :")
 		if choice == 'quit':
 			print "Thanks for using the address book. Goodbye!"
 			break
 			
 		elif choice == 'add':
-			print "Add entry to address book"
+			print "Add entry to address book: "
 			
-			new_contact = add_to_list()
-			address_book.append(new_contact)
-			print "added"
+			add_to_list(address_book)
 			
+		
 		elif choice == 'delete':
 			print "Delete entry from address book"
-			#contact_to_delete = delete_contact()
-			#address_book.remove(contact_to_delete)
 			
+			delete_contact(address_book)
 			
 		elif choice == 'list':
 			print "Listing entries from address book"
 			print address_book
 			
+		elif choice == "Save":
+			print "Save list"
+			save_list(address_book)
 			
+		elif choice == "load":
+			print "Load list"
+			load_file(address_book)	
+				
 		else: 
 			print "I'm sorry I don't know that command"
 			
