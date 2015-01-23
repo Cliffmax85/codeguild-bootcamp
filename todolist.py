5b4f42d62493f98e18f498c1640f960cdd6ffb8a
Clifford Maxson <cliffmax85@gmail.com>
2015-01-22 21:33:04 -0800

    initial commit

---
+++ todolist.py
@@ -0,0 +1,23 @@
+class ToDoListStorage(object):
+	"""Used by TodoList, this class takes care of saving a loading todo lists"""
+	def __init__(
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TodoList(object):
+	"""Manage a list of ToDo items"""
+	pass
+	
+class TodoItem(object):
+   """Manage a single ToDo item"""
+   pass	
